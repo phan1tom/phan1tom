1968e3d99241c2fa411be320ef49680ba1215081
phan1tom <120582021+phan1tom@users.noreply.github.com>
2023-11-12 20:49:04 +0500

    Create l2.c

---
+++ ver5/l2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+typedef unsigned char bool;
+
+bool is_input_valid(double x);
+
+int main()
+{
+    double x;
+    printf("Enter x: ");
+    scanf("%lf", &x);
+
+    if (is_input_valid(x))
+    {
+        double y = sqrt(sin(2 * x)) + sqrt(sin(3 * x));
+        double z = sqrt(sqrt( log(tan( y - (3.14 / 8) )) ));
+
+        printf("  y(x)  = %8lf\nz(y(x)) = %8lf\n", y, z);
+    }
+    else 
+    {
+        printf("Введённый x вне области определения!\n");
+    }
+
+    return 0;
+}
+
+bool is_input_valid(double x)
+{
+    int n = (int)(x / 3.14) / 2;
+    double remainder = x - (3.14 * 2 * n);
+    return 0.14352 < remainder && remainder < 1.02834;
+}
