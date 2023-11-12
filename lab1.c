90f67f25d36357d492f36a39fbe7d4eeda91e82d
phan1tom <120582021+phan1tom@users.noreply.github.com>
2023-11-12 20:34:54 +0500

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -1,33 +1,61 @@
-#include <stdio.h>
-#include <math.h>
-
+# include <stdio.h>
+# include <math.h>
 int main()
 {
-  float e1, e2, e3, Chast;
-  printf ("Enter e1 ->");
-  scanf ("%f", &e1);
-  printf ("Enter e2 ->");
-  scanf ("%f", &e2);
-  printf ("Enter e3 ->");
-  scanf ("%f", &e3);
-  float min = e1;
-  if (min >= e2)
+float a, b, c;
+printf ("Enter a ->");
+scanf ("%f", &a);
+printf ("Enter b ->");
+scanf ("%f ", & b);
+printf (" Enter c ->");
+scanf ("%f ", & c);
+if (! a && ! b && ! c)
+return 0;
+float x = 1.0f;
+printf (" f (x) = ");
+if (a)
+{
+printf ("%f * x ^2" , a );
+if (b > 0.0f)
+printf ("+");
+}
+if (b)
+printf ("%f * x " , b );
+if (c)
+{
+if (c > 0.0f)
+{
+if (a || b)
+printf ("+");
+else
+printf ("-");
+}
+printf ("% f " , fabs ( c ) );
+}
+printf ("\n");
+float f_x = a * x * x + b * x + c;
+float f_minus_x = a * -x * -x + b * -x + c;
+if (f_x == f_minus_x)
+printf ("Function is even : f (x) = %f = f ( - x ) = %f \n" , f_x ,
+f_minus_x);
+else
+if (f_minus_x == - f_x)
+printf ("Function is odd : f ( - x ) = %f = -f ( x ) = %f \n " ,
+
+f_minus_x , - f_x);
+
+else
+printf ("Function is neither even nor odd : f ( x ) = %f != f( - x ) = %f != -f ( x ) = %f \n " , f_x , f_minus_x , - f_x );
+
+if (a)
   {
-    min = e2;
-  }
-  if (min >= e3)
-  {
-    min = e3;
-  }
-  float Sum = e1 + e2 + e3;
-  if(Sum > 0)
-  {
-    Chast = min / Sum;
-    printf("Частное: %f; Сумма: %f\n", Chast, Sum);
-  }
-  else
-  {
-    Chast = Sum / min;
-    printf("Частное: %f; Сумма: %f\n", Chast, Sum);
-  }
+float vx = -b / (2.0f * a);
+float vy = a * vx * vx + b * vx + c;
+printf (" Vertex (% f , % f ) is a " , vx , vy);
+if ( a > 0.0f )
+printf ("minimum");
+else
+printf (" maximum ");
+   }
+   return 0;
 }
